f667ec571147b4703d1a0f3bdc95990c46fd926b
Максим Баранов <mbaranov@detmir.ru>
2021-12-27 22:07:53 +0300

    Initial commit

---
+++ src/main/java/com/maksimbb52/weaver/WeaverApplication.java
@@ -0,0 +1,13 @@
+package com.maksimbb52.weaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeaverApplication.class, args);
+    }
+
+}
